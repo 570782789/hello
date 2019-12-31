5a43ed559523806d8fbd94cecfe6f515b634beef
570782789 <570782789@qq.com>
2019-12-31 15:47:12 +0800

    commit

---
+++ src/igeek/hello.java
@@ -0,0 +1,14 @@
+package igeek;
+
+/**
+ * @Author:徐成
+ * @Create:2019/12/31 15:36
+ * @Description:
+ * @Modify By:dell
+ * @Version:
+ */
+public class hello {
+    public static void main(String[] args) {
+        System.out.println("123");
+    }
+}
